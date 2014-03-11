2d21c5ba98755fcf44ba00a594f14dab12c1325c
Jacob <obruza@gmail.com>
2014-03-11 13:23:48 -0500

    Fixed a few of my errors

--- src/com/unlucky4ever/removeexp/RemoveEXP.java
+++ src/com/unlucky4ever/removeexp/RemoveEXP.java
@@ -1,27 +1,29 @@
 package com.unlucky4ever.removeexp;
 
 import org.bukkit.event.Listener;
-import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.block.BlockExpEvent;
 import org.bukkit.event.entity.EntityDeathEvent;
+import org.bukkit.event.inventory.FurnaceExtractEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class RemoveEXP extends JavaPlugin {
 	
 	@Override
 	public void onEnable() {
-		getServer().getPluginManager().registerEvents(new EntityListener(), this);
-		getServer().getPluginManager().registerEvents(new BlockListener(), this);
+		getServer().getPluginManager().registerEvents(new RemoveEXPListener(), this);
 	}
 	
-	public class EntityListener implements Listener {
+	public class RemoveEXPListener implements Listener {
+		public void onBlockBreak(BlockExpEvent event) {
+			event.setExpToDrop(0);
+		}
+		
+		public void onFurnaceExtract(FurnaceExtractEvent event) {
+			event.setExpToDrop(0);
+		}
+		
 		public void onEntityDeath(EntityDeathEvent event) {
 			event.setDroppedExp(0);
-		}
-	}
-	
-	public class BlockListener implements Listener {
-		public void onBlockBreak(BlockBreakEvent event) {
-			event.setExpToDrop(0);
 		}
 	}
 }
